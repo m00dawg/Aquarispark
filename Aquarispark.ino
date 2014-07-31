1b7a9ca94fe555b1059adcc377b4bd013ee19363
Tim Soderstrom <tim@moocowproductions.org>
2014-07-31 12:26:52 -0500

    Added stats output string (Spark API Call)

--- Aquarispark.ino
+++ Aquarispark.ino
@@ -1,8 +1,7 @@
 /*
-	Aquarispark v1.00
-	By: Tim Soderstrom
+    Aquarispark v1.00
+    By: Tim Soderstrom
 */
-
 #include "OneWire.h"
 #include "DallasTemperature.h"
 
@@ -75,7 +74,7 @@ DeviceAddress tankThermometer;
 
 DeviceOnSchedule light = 
 {
-	  { 8, 0, 21, 0 }, // 8am - 9pm CST
+      { 8, 0, 21, 0 }, // 8am - 9pm CST
 //  { 3, 0, 16, 0 }, // 8am - 9pm CST (converted to UTC)
   lightPin,
   true                 // State
@@ -103,6 +102,9 @@ double currentTemp = 0;
 boolean heater = FALSE;
 int heaterCycles = 0;
 
+//String stats;
+char stats[12];
+
 /* Switches from C to F for display */
 boolean displayCelsius = true;
 
@@ -117,49 +119,58 @@ void setup()
   digitalWrite(heaterPin, LOW);
 
   //Sync the Time
-	Time.zone(timezone);
+  Time.zone(timezone);
   Spark.syncTime();
-	
-	// Subscribe variables to the cloud
-	Spark.variable("temperature", &currentTemp, DOUBLE);
-	Spark.variable("heater", &heater, INT);
-	Spark.variable("light", &light.state, INT);
-	
-	// Turn off that incredibly bright LED
-	RGB.control(true);
-	RGB.color(0, 255, 0);
-	RGB.brightness(32);
+    
+  // Subscribe variables to the cloud
+  Spark.variable("temperature", &currentTemp, DOUBLE);
+  Spark.variable("heater", &heater, INT);
+  Spark.variable("light", &light.state, INT);
+  Spark.variable("stats", &stats, STRING);
+    
+  // Turn off that incredibly bright LED
+  RGB.control(true);
+  RGB.color(0, 255, 0);
+  RGB.brightness(32);
 }
 
 void loop()
 {
 //  currentMillis = millis() + (timezone * SECS_PER_HOUR * msInSecond);
-	currentMillis = millis();
+    currentMillis = millis();
 
   /* If it's been longer than the polling interval, poll sensors */
   if (currentMillis - lastSensorPoll > sensorPollingInterval * msInSecond)
   {
-		Serial.print("Time: ");
-		Serial.print(Time.hour());
-		Serial.print(":");
-		Serial.println(Time.minute());
-		
+    Serial.print("Time: ");
+    Serial.print(Time.hour());
+    Serial.print(":");
+    Serial.println(Time.minute());
+        
     if(collectTemperatures())
       controlHeater();
     else
-			Serial.println("NO SENSORS");
+            Serial.println("NO SENSORS");
     
     // Check to see if it's time for light to turn on/off
-		if(checkSchedule(light))
-			Serial.println("Light On");
-		else
-			Serial.println("Light Off");
+    if(checkSchedule(light))
+      Serial.println("Light On");
+    else
+      Serial.println("Light Off");
+    
+    // Stats Output for API
+    sprintf(stats, "%.2f", currentTemp);
+    strcat(stats, ":");
+    strcat(stats, boolToChar(heater));
+    strcat(stats, ":");
+    strcat(stats, boolToChar(light.state));
+    
     lastSensorPoll = currentMillis;
-	}
-	if (currentMillis - lastTimeSync > oneDayMillis)
-	{
+  }
+  if (currentMillis - lastTimeSync > oneDayMillis)
+  {
     // Request time synchronization from the Spark Cloud
-		Serial.println("Syncing Time");
+        Serial.println("Syncing Time");
     Spark.syncTime();
     lastTimeSync = millis();
   }
@@ -172,8 +183,8 @@ boolean collectTemperatures()
   if(sensors.getAddress(tankThermometer, 0))
   {
     currentTemp = sensors.getTempC(tankThermometer);
-		Serial.print("Current Temperature: ");
-		Serial.println(currentTemp);
+        Serial.print("Current Temperature: ");
+        Serial.println(currentTemp);
     /* Check to see if we hit a new low or high temp */
     if(currentTemp > maxTemp)
       maxTemp = currentTemp;
@@ -189,7 +200,7 @@ void controlHeater()
   /* If temperature is too high, turn off heater */
   if(currentTemp > highTemp)
   {
-	  Serial.println("Heater Off");
+      Serial.println("Heater Off");
     digitalWrite(heaterPin, LOW);
     /* If the heater was on, we know we have completed a 
      * cycle, so let's count it */
@@ -201,8 +212,15 @@ void controlHeater()
   /* If the temperature is too low, turn on heater */
   if(currentTemp < lowTemp)
   {
-	  Serial.println("Heater On");
+      Serial.println("Heater On");
     digitalWrite(heaterPin, HIGH);
     heater = true;
   }
+}
+
+char* boolToChar(boolean b)
+{
+    if(b)
+        return "1";
+    return "0";
 }
